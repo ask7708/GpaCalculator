f927604e5460169d016ecea5fa7f119fac86d549
ask7708 <ask7708@rit.edu>
2015-05-26 20:15:04 -0400

    Created a base frame

--- src/calcGPA/GpaView.java
+++ src/calcGPA/GpaView.java
@@ -1,9 +1,34 @@
 package calcGPA;
+
 import javax.swing.*;
 
+public class GpaView extends JFrame {
 
-public class GpaView {
+	
+	public GpaView(){
+		
+		
+		
+		setSize(250, 400);
+		setTitle("GPA Calculator");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+	}
 	
 	
+	
+	
+	/**
+	 * Displays the scramble pad GUI
+	 * 
+	 * @param args
+	 *            - unused
+	 */
+	public static void main(String[] args) {
+
+		GpaView testWindow = new GpaView();
+		testWindow.setVisible(true);
+
+	}
 
 }
