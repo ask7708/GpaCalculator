6aada545864fbde414b9623f3f0cc8ce9c238790
ask7708 <ask7708@rit.edu>
2015-05-28 21:40:46 -0400

    Added columns with predefined grades

--- src/calcGPA/GpaView.java
+++ src/calcGPA/GpaView.java
@@ -1,14 +1,37 @@
 package calcGPA;
 
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.GridLayout;
+import java.util.ArrayList;
+
 import javax.swing.*;
 
 public class GpaView extends JFrame {
 
 	
+	/**
+	 * The array of letter grades
+	 */
+	public final String[] LETGRADES = {"A",  "A-", "B+", "B",
+			"B-", "C+", "C",  "D",  "D-", "E",  "F"};
+	
+	/**
+	 * An arraylist for storing user entered courses
+	 */
+	ArrayList<String> courseList = new ArrayList<String>();
+	
+	
+	/**
+	 * 
+	 * Used to set up the GUI, adds all the different 
+	 * components to the frame.
+	 * 
+	 */
 	public GpaView(){
 		
 		
-		
+		add(gradeList(), BorderLayout.CENTER );
 		setSize(250, 400);
 		setTitle("GPA Calculator");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -16,10 +39,29 @@ public class GpaView extends JFrame {
 	}
 	
 	
+	public Component gradeList(){
+		
+		JPanel gradePanel = new JPanel();
+		GridLayout gradeLayout = new GridLayout(6,0);
+	
+		gradePanel.setLayout(gradeLayout);
+		gradePanel.add(new JComboBox(LETGRADES));
+		gradePanel.add(new JComboBox(LETGRADES));
+		gradePanel.add(new JComboBox(LETGRADES));
+		gradePanel.add(new JComboBox(LETGRADES));
+		gradePanel.add(new JComboBox(LETGRADES));
+		gradePanel.add(new JComboBox(LETGRADES));
+		gradePanel.setVisible(true);
+		
+		return gradePanel;
+		
+	}
+	
+	
 	
 	
 	/**
-	 * Displays the scramble pad GUI
+	 * Displays the GPA GUI
 	 * 
 	 * @param args
 	 *            - unused
